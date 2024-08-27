bfa5e8e4483ddd242004431256d4afd86365fd8b
Chris <zhris@zhr.is>
2024-08-27 16:29:47 -0500

    Modified ContentView to add Circle graphic and animations, dynamic button text and count down animation.

--- BrushTimer/ContentView.swift
+++ BrushTimer/ContentView.swift
@@ -8,12 +8,44 @@ import SwiftUI
 struct ContentView: View {
     // Variables
     @State private var showingSettings = false
+    @State private var started = false
+    @State private var timeRemaining = 120
+    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
     
     var body: some View {
         VStack(spacing: 20) {
-            Text("Ready?")
-            Button("Start") {}
-                .buttonStyle(.borderedProminent)
+            if started {
+                ZStack {
+                    Circle()
+                        .trim(from: 0, to: CGFloat(timeRemaining)/120.0)
+                        .stroke(style: StrokeStyle(lineWidth: 16.0))
+                        .foregroundColor(Color.blue)
+                        .padding(50)
+                        .rotationEffect(.degrees(270))
+                    Text("\(timeRemaining)")
+                        .font(.system(size: 64))
+                        .fontWeight(.bold)
+                        .monospaced()
+                        .contentTransition(.numericText(countsDown: true))
+                        .animation(.default, value: timeRemaining)
+                        .onReceive(timer) { time in
+                            if timeRemaining > 0 && started {
+                                withAnimation {
+                                    timeRemaining -= 1
+                                }
+                            }
+                        }
+                }
+            }
+            if !started {
+                Text(timeRemaining < 120 ? "Continue?" : "Ready?")
+            }
+            Button(started ? "Pause" : timeRemaining < 120 ? "Resume" : "Start") {
+                withAnimation {
+                    started.toggle()
+                }
+            }
+            .buttonStyle(.borderedProminent)
         }
         .navigationTitle("BrushTimer")
         .navigationBarTitleDisplayMode(.inline)
